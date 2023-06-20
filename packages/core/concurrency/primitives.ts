0b836045a698c024cfeb8ca45d5cec3fc8233d26
nathannorthcutt <40191617+nathannorthcutt@users.noreply.github.com>
2023-06-20 03:11:28 +0000

    local save

--- packages/core/concurrency/primitives.ts
+++ packages/core/concurrency/primitives.ts
@@ -14,14 +14,14 @@ export class Mutex {
 
     private locked: boolean = false
     private callbacks: MutexCallback[] = []
-    
+
     /**
      * Tries to acquire the {@link Mutex} but will not block if unavailable
      * 
      * @returns True if the mutex was acquired
      */
     tryAcquire(): boolean {
-        if(!this.locked){
+        if (!this.locked) {
             this.locked = true
             return true
         }
@@ -35,7 +35,7 @@ export class Mutex {
      * @returns A {@link PromiseLike} value for tracking when the mutex is acquired
      */
     acquire(): PromiseLike<void> {
-        if(!this.locked){
+        if (!this.locked) {
             this.locked = true
             return Promise.resolve()
         }
@@ -51,7 +51,7 @@ export class Mutex {
     release(): void {
         this.locked = false
 
-        if(this.callbacks.length > 0){
+        if (this.callbacks.length > 0) {
             setImmediate(this.callbacks.shift()!)
         }
     }
@@ -66,13 +66,13 @@ const MONITOR_SYMBOL = Symbol('_mon_')
 export class Monitor {
 
     private mutex: Mutex = new Mutex()
-    
+
     /**
      * Wait for the given {@link Monitor} to become available
      * 
      * @returns A {@link PromiseLike} value that can be used to `await` the underly resource being available
      */
-    wait(): PromiseLike<void>{
+    wait(): PromiseLike<void> {
         return this.mutex.acquire()
     }
 
@@ -89,8 +89,56 @@ export class Monitor {
  * @param obj The object to get a monitor for
  * @returns The {@link Monitor} associated with the object
  */
-export function getMonitor(obj: any): Monitor{
+export function getMonitor(obj: any): Monitor {
 
     // Get the monitor or inject it
     return obj[MONITOR_SYMBOL] === undefined ? (obj[MONITOR_SYMBOL] = new Monitor()) : obj[MONITOR_SYMBOL] as Monitor
+}
+
+/**
+ * Represents a semaphore that can be used to control concurrent actions
+ */
+export class Semaphore {
+    private concurrency: number
+    private running: number = 0
+    private callbacks: MutexCallback[] = []
+
+    /**
+     * 
+     * @param concurrency The desired concurrency
+     */
+    constructor(concurrency: number) {
+        this.concurrency = concurrency
+    }
+
+    async run<T>(fn: () => Promise<T>): Promise<T> {
+
+    }
+
+    private acquire(): Promise<void> | undefined {
+
+        // Go ahead and run
+        if(this.running < this.concurrency){
+            this.running++
+            return
+        }
+
+        const p = new Promise(resolve=>{
+            
+        })
+    }
+
+    /**
+     * @returns The number of available slots in the semaphore
+     */
+    available(): number {
+        return this.concurrency - this.running
+    }
+
+    /**
+     * @returns The current concurrency limit
+     */
+    limit(): number {
+        return this.concurrency
+    }
 }
