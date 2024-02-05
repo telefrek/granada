6bb4d87cdc24a0c69f944cf8c5f76b5893d947a7
Nathan Northcutt <nathan@telefrek.com>
2024-02-04 19:02:36 -0800

    trying to get lerna hooked up

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,20 +2,6 @@
 export type MaybeAwaitable<T> = T | PromiseLike<T>
 
 /**
- * Check to see if the target is {@link PromiseLike}
- * @param target The object to inspect
- * @returns True if it is {@link PromiseLike}
- */
-export function isPromiseLike<T>(target: unknown): target is PromiseLike<T> {
-  return (
-    typeof target === "object" &&
-    target !== null &&
-    "then" in target &&
-    typeof target.then === "function"
-  )
-}
-
-/**
  * Checks if th eobject is empty
  * @param target The object to inspect
  * @returns true if the object has no properties
