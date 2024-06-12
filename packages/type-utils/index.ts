582c49d482950afce4fa3731baf9ffbe49a6e1de
Nathan Northcutt <nathan@telefrek.com>
2024-06-12 16:04:33 +1200

    rename to make more sense

--- packages/type-utils/index.ts
+++ packages/type-utils/index.ts
@@ -2,12 +2,15 @@
  * Common Type utilities
  */
 
-type _<T> = T
+/**
+ * Utility type to return back what was given
+ */
+type Extract<T> = T
 
 /**
  * Flatten the definition (highly useful for combinations)
  */
-export type Flatten<T> = _<{ [K in keyof T]: T[K] }>
+export type Flatten<T> = Extract<{ [K in keyof T]: T[K] }>
 
 /**
  * Type for passing invalid typings since there is no way to do it currently
